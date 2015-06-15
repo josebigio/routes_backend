a6d600c00323d971bfd006b7c5b0b2c9ac9be0ef
Jose Bigio <josebigio@Joses-Air.attlocal.net>
2015-06-14 22:48:02 -0500

    added a config

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	print(os.environ['APP_SETTINGS'])
+	return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+	app.run()

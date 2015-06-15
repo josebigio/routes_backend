40a13d3ea09eec5e70cb8ae4ed5e22b4a0e8864a
Jose Bigio <josebigio@Joses-MacBook-Air.local>
2015-06-14 23:00:15 -0500

    df

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-	print(os.environ['APP_SETTINGS'])
 	return "Hello World!"
 
 if __name__ == '__main__':
+	print(os.environ['APP_SETTINGS'])
 	app.run()

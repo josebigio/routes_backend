5cb5acbd5aae247d64bebe082db4bb064d6bf6ab
Jose Bigio <josebigio@Joses-Air.attlocal.net>
2015-06-17 17:03:31 -0500

    got a basic data base working. Its super slow though

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from geopy.distance import vincenty
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import Stops
+import mainProcessor
 
 @app.route('/')
 def hello():
-	# workPlease = stops.query.all()
-	# print(workPlease)
 	return "Hello World!"
 
+@app.route('/api/getroutes',methods=['GET'])
+def getRoutes():
+	lat = request.args.get('lat')
+	lng = request.args.get('lng')
+	radius = request.args.get('radius')
+
+	stops = mainProcessor.getStopIds(lat,lng,radius)
+	resultDict = dict()
+	for stop in stops:
+		stopId = stop.stop_id
+		routes = mainProcessor.getRoutesWithStopID(stopId)
+		routeNames = [route.route_id for route in routes]
+		resultDict[stopId] = [routeNames,mainProcessor.getDistance(lat,lng,stop.stop_lat,stop.stop_lon)]
+		
+
+	return jsonify(resultDict)
+
+@app.route('/api/getroutesusingstopid',methods=['GET'])
+def getroutesusingstopid():
+	stopId = request.args.get('stopId')
+	result = mainProcessor.getRoutesWithStopID(stopId)
+	resultDict = dict()
+	index = 0
+	for route in result:
+		resultDict[route.route_id] = route.route_long_name
+
+	return jsonify(resultDict)
+
+@app.route('/api/getstopidsaround',methods=['GET'])
+def getStopIdsAround():
+	lat = request.args.get('lat')
+	lng = request.args.get('lng')
+	radius = request.args.get('radius')
+	result = mainProcessor.getStopIds(lat,lng,radius)
+	resultDict = dict()
+	index = 0
+	for stop in result:
+		resultDict[stop.stop_id] = mainProcessor.getDistance(lat,lng,stop.stop_lat,stop.stop_lon)
+
+	return jsonify(resultDict)
+	
 if __name__ == '__main__':
 	app.run()
--- mainProcessor.py
+++ mainProcessor.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python
+
 from geopy.distance import vincenty
 import random
+from app import app
+import models
 
 
 DEBUG = 0
 
 def getRouteWithTripId(tripId):
-	tripId = str(tripId)
-	file = open("GTFS/trips.txt")
-	for line in file:
-		lineArr = line.split(',')
-		if tripId in lineArr[2]:
-			routeId = lineArr[0]
-			if DEBUG:
-				print("found routeId: %s" % routeId)
-			return routeId
+	trip =  models.Trips.query.filter_by(trip_id=tripId).first()
+	return models.Routes.query.filter_by(route_id=trip.route_id).first()
+
 
 def getRoutesWithStopID(stopId):
 	stopId = str(stopId)
-	print("requested stopId: %s" % stopId)
-
 	routeIdSet = set()
-	file = open("GTFS/stop_times.txt")
-	for line in file:
-		lineArr = line.split(',')
-		stopIdR = lineArr[3]
-		if stopIdR in stopId:
-			tripId = lineArr[0]
-			if DEBUG:
-				print("found tripId %s" % tripId)
-			routeIdSet.add(getRouteWithTripId(tripId))
-
-	return list(routeIdSet)
+	#stopTimes has all the trips that go stopId
+	stopTimes = models.StopTimes.query.filter_by(stop_id=stopId).all()
+	for stopTime in stopTimes:
+		routeIdSet.add(getRouteWithTripId(stopTime.trip_id))
+	
+	return routeIdSet
 
 def getDistance(lat1,long1,lat2,long2):
 	loc1 = (lat1,long1)
@@ -39,24 +29,28 @@ def getDistance(lat1,long1,lat2,long2):
 	return vincenty(loc1, loc2).miles
 	
 
-def getBusStops(lat, lng, radius):
-	print("requested lat,lng: " + str(lat) + ", " + str(lng))
-	file = open("GTFS/stops.txt")
-	file.readline()
-	for line in file:
-			lineArr = line.split(',')
-			latR = str(lineArr[4])
-			lngR = str(lineArr[5])
-			distance = getDistance(lat,lng,float(latR),float(lngR))
-			if distance<radius:
-				stopId = lineArr[0]
-				routes = getRoutesWithStopID(stopId)
-				print("distance: %f; name: %s"%(distance,lineArr[2]))
-				print(routes)	
+def getRoutes(lat, lng, radius):
+	stops = getStopIds(lat,lng,radius)
+	resultList = list()
+	for stop in stops:
+		stopId = stop.stop_id
+		resultList.append(getRoutesWithStopID(stopId))
 
+	return resultList
+		
 
+def getStopIds(lat, lng, radius):
+	
+	result = models.Stops.query.all()
+	radius = float(radius)
 
+	resultList = list()
+	for stop in result:
+		distance = getDistance(lat,lng,stop.stop_lat,stop.stop_lon)
+		if distance < radius:
+			resultList.append(stop)
 
-getBusStops(30.2642580,-97.7323400,0.5)
+	
+	return resultList
 
 

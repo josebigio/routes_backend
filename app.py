207c587cbef17fc6f0674db222036d8557e8d0cc
Jose Bigio <josebigio@Joses-Air.attlocal.net>
2015-06-21 23:29:25 -0500

    added lat,lng

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def getRoutes():
 		stopId = stop.stop_id
 		routes = mainProcessor.getUpcomingRoutesWithStopId(stopId,time,limit,weekday)
 		distance = mainProcessor.getDistance(lat,lng,stop.stop_lat,stop.stop_lon)
-		d = {"stop_id":stopId, "routes":routes,"distance":distance}
+		d = {"stop_id":stopId, "routes":routes,"distance":distance,"lat":stop.stop_lat,"lng":stop.stop_lon}
 		resultList.append(d)
 		
 	resultDict = {"stops":resultList}
